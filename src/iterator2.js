fbee4e02e4f5e417060d6b4797d57b91a1460fee
Jeremy Latt <jeremy.latt@gmail.com>
2013-05-21 23:24:36 -0700

    docs

--- src/iterator2.js
+++ src/iterator2.js
@@ -1,21 +1,26 @@
+// Do nothing.
 function noop(value, send) {}
 
+// Send every received value.
 function identity(value, send) {
     send.call(this, value);
 }
 
+// Map incoming values with `func(value)`.
 function map(func) {
     return function(value, send) {
         send.call(this, func.call(this, value));
     };
 }
 
+// Map incoming values by applying `value` an an arguments array.
 function mapApply(func) {
     return function(value, send) {
         send.call(this, func.apply(this, value));
     };
 }
 
+// Filter incoming values with `func(value)`.
 function filter(func) {
     return function(value, send) {
         if (func.call(this, value)) {
@@ -24,6 +29,9 @@ function filter(func) {
     };
 }
 
+// Fold over the incoming stream. Call `func(current, value)` where `current`
+// begins with value `initial` and the return value sets `current` for the next
+// call.
 function fold(initial, func) {
     var current = initial;
     return function(value, send) {
@@ -32,8 +40,15 @@ function fold(initial, func) {
     };
 }
 
+// Chain a list of iterator functions together.
 function chain(/*iterator, ...*/) {
-    frp.assert(arguments.length > 1);
+    if (arguments.length === 0) {
+        return identity;
+    }
+
+    if (arguments.length === 1) {
+        return arguments[0];
+    }
 
     return _.reduceRight(arguments, function(next, current) {
         return function(value, send) {
@@ -44,12 +59,15 @@ function chain(/*iterator, ...*/) {
     }, identity);
 }
 
+// Send a constant `value` for every received value.
 function constant(value) {
     return map(function() {
         return value;
     });
 }
 
+// For incoming values, send an array of up to `n` received values in that
+// order.
 function lastN(n) {
     frp.assert(n > 1);
 
@@ -63,6 +81,7 @@ function lastN(n) {
     });
 }
 
+// For the first value, call `once`. Afterwards, call `then`.
 function onceThen(once, then) {
     var current = function() {
         once.apply(this, arguments);
@@ -73,6 +92,7 @@ function onceThen(once, then) {
     };
 }
 
+// Only send values unique from the previously received value.
 function unique(isEqual) {
     if (!_.isFunction(isEqual)) {
         isEqual = _.bind(_.isEqual, _);
@@ -90,6 +110,7 @@ function unique(isEqual) {
     });
 }
 
+// Send received values until `func` returns `false`.
 function takeWhile(func) {
     var taking = function(value, send) {
         if (func.call(this, value)) {
@@ -103,6 +124,7 @@ function takeWhile(func) {
     };
 }
 
+// Send no received values until `func` returns `false`.
 function dropWhile(func) {
     var dropping = function(value, send) {
         if (!func.call(this, value)) {
@@ -115,43 +137,47 @@ function dropWhile(func) {
     };
 }
 
+// Send the last value after reeiving no values for `wait` ms.
 function debounce(wait) {
     return _.debounce(identity, wait);
 }
 
+// Send no more than one value per `wait` ms.
 function throttle(wait) {
     return _.throttle(identity, wait);
 }
 
+// Send incoming values after a delay of `wait` ms. This relies on the
+// scheduler, so order cannot be guaranteed.
 function delay(wait) {
     var handle;
-    function makeDelay(value, send) {
-        handle = _.chain(send).bind(this, [value]).delay(wait).value();
-    }
-    return onceThen(makeDelay, function(value, send) {
-        window.clearTimeout(handle);
-        makeDelay.apply(this, arguments);
-    });
+    return function(value, send) {
+        _.chain(send).bind(this, [value]).delay(wait);
+    };
 }
 
+// Turn incoming values into a promise for a value.
 var promise = map(function() {
     return jQuery.Deferred().resolveWith(this, arguments).promise();
 });
 
+// Remove promise wrappers. Order is not guaranteed.
 function unpromise(promise, send) {
     promise.done(send);
 }
 
-var abortLastPromise = chain(lastN(2), mapApply(function(current, last) {
+// Call `abort` on the previous value sent when sending a new value.
+var abortLast = chain(lastN(2), mapApply(function(current, last) {
     if (arguments.length > 1) {
         last.abort();
     }
     return current;
 }));
 
-function mapPromise(done, fail) {
+// Map promises through a filter. Order is not guaranteed.
+function mapPromise(done) {
     return map(function(promise) {
-        return promise.then(done, fail);
+        return promise.then(done);
     });
 }
 
@@ -160,24 +186,24 @@ function mapPromise(done, fail) {
 //
 
 frp.iter = {
-    'abortLastPromise': abortLastPromise,
-    'chain':            chain,
-    'constant':         constant,
-    'debounce':         debounce,
-    'delay':            delay,
-    'dropWhile':        dropWhile,
-    'filter':           filter,
-    'fold':             fold,
-    'identity':         identity,
-    'lastN':            lastN,
-    'map':              map,
-    'mapPromise':       mapPromise,
-    'mapApply':         mapApply,
-    'noop':             noop,
-    'onceThen':         onceThen,
-    'promise':          promise,
-    'takeWhile':        takeWhile,
-    'throttle':         throttle,
-    'unique':           unique,
-    'unpromise':        unpromise
+    'abortLast':  abortLast,
+    'chain':      chain,
+    'constant':   constant,
+    'debounce':   debounce,
+    'delay':      delay,
+    'dropWhile':  dropWhile,
+    'filter':     filter,
+    'fold':       fold,
+    'identity':   identity,
+    'lastN':      lastN,
+    'map':        map,
+    'mapPromise': mapPromise,
+    'mapApply':   mapApply,
+    'noop':       noop,
+    'onceThen':   onceThen,
+    'promise':    promise,
+    'takeWhile':  takeWhile,
+    'throttle':   throttle,
+    'unique':     unique,
+    'unpromise':  unpromise
 };
