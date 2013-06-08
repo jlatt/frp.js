a9d78c2142c64bbb389acea57d3184049a178fa6
Jeremy Latt <jeremy.latt@gmail.com>
2013-06-08 10:51:58 -0700

    add documentation

--- src/util.js
+++ src/util.js
@@ -1,22 +1,34 @@
+// If `condition` is falsy, throw an error.
+//
+// condition := Value
+// message := String [optional]
+// throws := Error
 function assert(condition, message) {
     if (!condition) {
         throw new Error(message || 'assertion failed');
     }
 }
 
-function getDefault(object, key, defValueFunc) {
+// Get a keyed value from an object. If the value is not present in the
+// prototype chain, call `makeDefaultValue`, setting it as `key` in `object` and
+// returning it.
+//
+// object := Object
+// key := String
+// makeDefaultValue := function(object, key, makeDefaultValue) Value
+function getDefault(object, key, makeDefaultValue) {
     if (key in object) {
         return object[key];
     }
-    var value = defValueFunc.apply(this, arguments);
+    var value = makeDefaultValue.apply(this, arguments);
     object[key] = value;
     return value;
 }
 
+// Create a prototypal heir of an object.
 //
-// prototypal inheritance
-//
-
+// object := Object
+// returns := Object
 function heir(object) {
     function Heir() {}
     Heir.prototype = object;
